30227fb7efc053674379df3ccc5197368d0c7c90
faustofusse <faustofusse@gmail.com>
2023-08-22 08:18:03 -0300

    feat: seleccionar secciones mediante args

--- main.go
+++ main.go
@@ -18,8 +18,14 @@ import (
 	"github.com/faiface/beep/speaker"
 )
 
-var ids []string = []string{ "I", "J", "F", "G", "H", "K" }
+var primera      []string = []string{ "LID", "LIC", "LII", "LPD", "LPC", "LPI", "LD", "LV" }
+var segunda      []string = []string{ "SDI", "SDD", "SBI", "SBC", "SBD", "SAI", "SAC", "SAD", "SCI", "SCD", "MI", "MC", "MD", "SMV" }
+var tercera      []string = []string{ "I", "J", "F", "G", "H", "K" }
+var torres       []string = []string{ "TS1", "TS2", "TS3", "TS4", "TN1", "TN2", "TN3", "TN4" }
+var palcos       []string = []string{ "PLCVN", "PLCVS", "PLCPREF", "PPS1", "PPS2", "PPS3", "PPN1", "PPN2", "PPN3" }
+var preferencial []string = []string{ "PRS1", "PRS2", "PRS3", "PRN1", "PRN2", "PRN3" }
 
+var ids []string
 var eNid string
 var sections []*cdp.Node = []*cdp.Node{}
 var previousSections []*cdp.Node = []*cdp.Node{}
@@ -92,12 +98,24 @@ func getArg(arg string) *string {
     return nil
 }
 
-func parseArgs() (username *string, password *string) {
+func parseSections(selected string) (result []string) {
+    if strings.Contains(selected, "1") { result = append(result, primera...) }
+    if strings.Contains(selected, "2") { result = append(result, segunda...) }
+    if strings.Contains(selected, "3") { result = append(result, tercera...) }
+    if strings.Contains(selected, "t") { result = append(result, torres...) }
+    if strings.Contains(selected, "p") { result = append(result, palcos...) }
+    if strings.Contains(selected, "P") { result = append(result, preferencial...) }
+    return result
+}
+
+func parseArgs() (username *string, password *string, selected *string) {
     if username = getArg("-u"); username == nil { username = getArg("--username") }
     if username == nil { log.Fatal("Username missing") }
     if password = getArg("-p"); password == nil { password = getArg("--password") }
     if password == nil { log.Fatal("Password missing") }
-    return username, password
+    if selected = getArg("-s"); selected == nil { selected = getArg("--sections") }
+    if selected == nil { log.Fatal("Selected sections missing") }
+    return username, password, selected
 }
 
 func parseEnid(ctx context.Context) error {
@@ -132,8 +150,7 @@ func gotoComprar() chromedp.Tasks {
 }
 
 func checkSeats(ctx context.Context) error {
-    var seats []*cdp.Node
-    var buttons []*cdp.Node
+    var seats, buttons []*cdp.Node
     for _, section := range sections {
         if !contains(ids, section.AttributeValue("id")) { continue }
         esNid := section.AttributeValue("data-nid")
@@ -171,19 +188,17 @@ func checkSections() chromedp.Tasks {
 }
 
 func main() {
-    username, password := parseArgs()
+    username, password, selected := parseArgs()
+
+    ids = parseSections(*selected)
 
     cancel := initSound()
     defer cancel()
 
-    dir, err := os.MkdirTemp("", "chromedp-example")
-    if err != nil { log.Fatal(err) }
-    defer os.RemoveAll(dir)
-
-    opts := append(chromedp.DefaultExecAllocatorOptions[:],
+    opts := append(
+        chromedp.DefaultExecAllocatorOptions[:],
         chromedp.Flag("headless", false),
         chromedp.DisableGPU,
-        chromedp.UserDataDir(dir),
     )
 
     allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
@@ -192,11 +207,11 @@ func main() {
     taskCtx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
     defer cancel()
 
-    err = chromedp.Run(taskCtx, loginTasks(username, password))
+    err := chromedp.Run(taskCtx, loginTasks(username, password))
     if err != nil { log.Fatal(err) }
 
     for {
         err = chromedp.Run(taskCtx, gotoComprar(), checkSections())
-        fmt.Printf("\nerror: %v", err.Error())
+        // fmt.Printf("\nerror: %v", err.Error())
     }
 }
